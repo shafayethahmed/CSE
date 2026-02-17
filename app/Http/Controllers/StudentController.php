434394ac769601f21ad490fb58b153f582c7bea1
Shafayeth Ahmed <dev.shafayeth@gmail.com>
2026-02-17 21:54:56 +0600

    Student Profile Controller method Created

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+      public function index(){
+        return view('students.index');  //Index Page Display.
+      }
+     
+     //student Create Form: 
+     public function create(){
+        return view(''); //Need to assign the Student Create Form.
+     }
+
+    //Storing the Student Information: 
+    public function store(){
+        //Process Student data for insert.
+    } 
+
+    //Edit form display for student:
+    public function edit(){
+        return view(''); //Need to assign the Student Edit Form.
+    }
+
+    //Information View Form for Student
+    public function show(){
+        //Information about Student Profile.
+    }
+    
+    //Student profile update.
+    public function update(){
+        //Update Process logic need to implement here.
+    }
+
+
+
 }

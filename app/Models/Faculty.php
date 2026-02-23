338d1716c4549cc2012072f17026e5fa4f4d9f27
Shafayeth Ahmed <dev.shafayeth@gmail.com>
2026-02-24 01:22:56 +0600

    faculty Model Updated

---
+++ app/Models/Faculty.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Faculty extends Model
+{
+      protected $table = 'faculty';
+
+    protected $fillable = [
+        'faculty_id',
+        'name',
+        'email',
+        'designation',
+        'credit_limit',
+        'bachelor_degree',
+        'bachelor_university',
+        'bachelor_cgpa',
+        'master_degree',
+        'master_university',
+        'master_cgpa',
+        'faculty_status',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+   
+}

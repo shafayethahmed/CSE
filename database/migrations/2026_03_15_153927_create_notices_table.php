092f559612f76027f53822be4ce35aec4f92ddf3
Shafayeth Ahmed <dev.shafayeth@gmail.com>
2026-03-15 21:44:38 +0600

    Notice Section working start

---
+++ database/migrations/2026_03_15_153927_create_notices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('body');
+            $table->string('publidhed_by',30);
+            $table->string('designation',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notices');
+    }
+};
